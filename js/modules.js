ff5877e19a4115428cd957301ee06a000d9e1a55
David Martinez Moll <info@dmmoll.com>
2015-07-14 17:48:36 -0300

    modules_update
    Product panel and working with the shopping cart

--- js/modules.js
+++ js/modules.js
@@ -50,7 +50,7 @@ CORE.create_module("filters-bar", function(sb){
 			filters = sb.find('a');
 			sb.addEvent(filters, "click", this.filterProducts);
 		},
-		destroy: funtion() {
+		destroy: function() {
 			sb.removeEvent(filters, "click", this.filterProducts);
 			filter = null;
 		},
@@ -89,8 +89,8 @@ CORE.create_module("product-panel", function(sb){
 			products = sb.find("li");
 			sb.listen({
 				'change-filter' : this.change_filter,
-				'reset-filter'  : this.reset_filter,
-				'perfrom-search': this.search,
+				'reset-filter'  : this.reset,
+				'perform-search': this.search,
 				'quit-search'   : this.reset
 			});
 			eachProduct(function(product) {
@@ -98,8 +98,67 @@ CORE.create_module("product-panel", function(sb){
 			});
 		},
 		destroy: function() {
-
+			var self = this;
+			eachProduct(function(product) {
+				sb.removeEvent(product, 'click', self.addToCart);
+			});
+			sb.ignore(['change-filter', 'reset_filter', 'perform-search', 'quit-search']);
+		},
+		reset: reset,
+		change_filter: function(filter) {
+			reset();
+			eachProduct(function(product) {
+				if (product.getAttribute("data-8099-keyword").toLowerCase().indexOf(filter.toLowerCase()) < 0) {
+					product.style.opacity = '0.2';
+				}
+			});
+		},
+		search: function(query) {
+			query = query.toLowerCase();
+			eachProduct(function(product) {
+				if (product.getElementsByTagName('p')[0].innerHTML.toLowerCase().indexOf(query.toLowerCase()) < 0) {
+					product.style.opacity = '0.2';
+				}
+			});	
+		},
+		addToCart: function(e) {
+			var li = e.currentTarget;
+			sb.notify({
+				type: 'add-item',
+				data: {id: li.id, name: li.getElementsById('p')[0].innerHTML, price: parseInt(li.id, 10) }
+			});
 		}
 	};
 });
 
+
+
+// Shopping cart
+/////////////////////
+
+CORE.create_module("shopping-cart", function(sb) {
+	var cart, cartItems;
+
+	return {
+		init: function() {
+			cart = sb.find("ul")[0];
+			cartItems = {};
+			sb.listen({
+				'add-item': this.addItem
+			});
+		},
+		destroy: function() {
+			cart = cartItems = null;
+			sb.ignore(['add-item']);
+		},
+		addItem : function (product) {
+			var entry = sb.find('#cart-' + product.id + ' .quantity')[0];
+			if(entry) {
+				entry.innerHTML = parse(entry.innerHTML, 10) + 1;
+				cartItems[product.id]++;
+			} else {
+				entry = sb.create_element("li", {});
+			}
+		}
+	};
+});
